24cd44b6d80c95765a47ca410a12ea41a13178e2
Sammi Turner <ms.sammi.turner@gmail.com>
2023-02-17 01:27:11 +0000

    Add files via upload

---
+++ axum_hello/src/main.rs
@@ -0,0 +1,29 @@
+#![allow(unused_imports)]
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse, Json, Router
+};
+
+use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
