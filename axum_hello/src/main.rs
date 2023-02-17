c2d248f17534f992d2a8f8778e1299534fffe30b
Sammi Turner <ms.sammi.turner@gmail.com>
2023-02-17 01:45:29 +0000

    Update main.rs

--- axum_hello/src/main.rs
+++ axum_hello/src/main.rs
@@ -16,7 +16,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
