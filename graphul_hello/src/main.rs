71822ea3b5e1ddbbd1b49487be4de02ff27a1c9e
Sammi Turner <ms.sammi.turner@gmail.com>
2023-02-17 01:32:00 +0000

    Add files via upload

---
+++ graphul_hello/src/main.rs
@@ -0,0 +1,8 @@
+use graphul::{http::Methods, Graphul};
+
+#[tokio::main]
+async fn main() {
+    let mut app = Graphul::new();
+    app.get("/", || async { "Hello, World 👋!" });
+    app.run("127.0.0.1:8000").await;
+}
